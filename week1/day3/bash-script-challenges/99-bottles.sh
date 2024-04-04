c759423c80a629b9af9660c3e98e287f9d871b63
Neka Blair <ourblairfamily@gmail.com>
2024-04-03 22:24:46 -0500

    formatted string

--- week1/day3/bash-script-challenges/99-bottles.sh
+++ week1/day3/bash-script-challenges/99-bottles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # echo "I work!"
 
-# mainsong="99 bottles of beer on the wall, 99 bottles of beer.
+# "99 bottles of beer on the wall, 99 bottles of beer.
 # Take one down and pass it around, 98 bottles of beer on the wall"
 
 # onebottle="Take one down and pass it around, 1 bottle of beer on the wall.
@@ -15,20 +15,19 @@ for ((i = 99; i >=1 ; i--)); do
     # echo "$i"
     numbottles=$i
     if [[ $numbottles -ge 3 ]]; then
-        echo "$numbottles bottles of beer on the wall, 
-        $numbottles bottles of beer.
-        Take one down and pass it around, 
-        $(( $numbottles-1 )) bottles of beer on the wall"
+        printf "\n$numbottles bottles of beer on the wall,
+        \n$numbottles bottles of beer.
+        \nTake one down and pass it around, 
+        \n$(($numbottles-1)) bottles of beer on the wall.\n"
     elif [[ $numbottles == 2 ]]; then
-        echo "$numbottles bottles of beer on the wall, 
-        $numbottles bottles of beer.
-        Take one down and pass it around, 
-        1 bottle of beer on the wall"
+        printf "\n$numbottles bottles of beer on the wall, 
+        \n$numbottles bottles of beer.
+        \nTake one down and pass it around, 
+        \n1 bottle of beer on the wall\n"
     else
-        echo "No more bottles of beer on the wall, 
-        no more bottles of beer.
-        Go to the store and buy some more, 
-        $((99)) bottles of beer on the wall."
+        printf "\nNo more bottles of beer on the wall, 
+        \nno more bottles of beer.
+        \nGo to the store and buy some more, 
+        \n$((99)) bottles of beer on the wall.\n"
     fi
 done
-# printf \n
